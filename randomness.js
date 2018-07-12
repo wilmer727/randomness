e1e0debac3594c67a5313d05d3711584fd3a760b
Wilmer <wilmer.bacuilima@uasdc.org>
2018-07-12 10:44:20 -0700

    randomness

---
+++ randomness.js
@@ -0,0 +1,42 @@
+//////////////////////
+// GLOBAL VARIABLES //
+//////////////////////
+// function randomNumber(){
+//     console.log (Math.floor(Math.random() * 21 + 1 ));
+// }
+//     randomNumber(); 
+
+
+
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+function randomLetter(){
+    console.log (alphabet.charAt(Math.random() * 26 + 1));
+    }
+    randomLetter();
+
+
+
+
+
+const foods = [
+    "pizza",
+    "burgers",
+    "rice and beans",
+    "lasagna",
+    "macaroni and cheese",
+    "baked potatoes"
+];
+const students = [
+    "Bob",
+    "Martin",
+    "Danny",
+    "Mahdi",
+    "Harry",
+    "Sean",
+    "Robert"
+];
+/////////////////////////////
+// END OF GLOBAL VARIABLES //
+/////////////////////////////
